c805517baa414966b6d1e001b9ea0d90e34990a5
Štěpán Záliš <stepan.zalis@applifting.cz>
2023-04-28 21:45:40 +0200

    feat: better example UI

---
+++ example/lib/counter_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
---
+++ example/lib/wishfly_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:wishfly/wishfly.dart';
+
+class WishflyScreen extends StatelessWidget {
+  const WishflyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Wishfly(
+      apiKey: "qerMWwyYfJlVkv5wFOA5OKK-z8tmU9rTI2P9RBraouc=", // Paste your API key here
+      projectId: 10, // Paste your project ID here
+      localizationOverrides: const {
+        "noWishes": "No wishes here, but you can be the first one!", // override specific key in localization
+      },
+      theme: WishflyThemeData.light(
+        voteIconColor: Colors.black,
+        addWishButtonColor: Colors.black,
+      ),
+    );
+  }
+}
